76ec3999f15f29a892224ba05b700c6305a0b833
stahh <lupuslone@hotmail.com>
2022-05-30 12:26:59 +0300

    fixed crash of whole app because downloader_1        |   File "/usr/local/lib/python3.9/site-packages/magnet2torrent/magnet2torrent.py", line 143, in retrieve_torrent downloader_1        |     result = task.result() downloader_1        |   File "/usr/local/lib/python3.9/site-packages/magnet2torrent/httptracker.py", line 35, in retrieve_peers_http_tracker downloader_1        |     result = bdecode(result) downloader_1        |   File "/usr/local/lib/python3.9/site-packages/magnet2torrent/bencode.py", line 69, in bdecode downloader_1        |     r, l = decode_func[six.indexbytes(x, 0)](x, 0) downloader_1        | KeyError: 60

--- src/magnet2torrent/httptracker.py
+++ src/magnet2torrent/httptracker.py
@@ -1,5 +1,6 @@
 import asyncio
 import struct
+import time
 from ipaddress import IPv4Address
 from urllib.parse import quote
 
@@ -10,29 +11,46 @@ from . import settings
 from .bencode import bdecode
 
 
-async def retrieve_peers_http_tracker(task_registry, tracker, infohash):
-    url = f"{tracker}?info_hash={quote(infohash)}&peer_id={quote(settings.PEER_ID)}&port={settings.BITTORRENT_PORT}&uploaded=0&downloaded=0&left=16384&compact=1&event=started&no_peer_id=1&numwant=200"
+async def retrieve_peers_http_tracker(task_registry, tracker, infohash, logger):
+    url = f"{tracker}?info_hash={quote(infohash)}" \
+          f"&peer_id={quote(settings.PEER_ID)}" \
+          f"&port={settings.BITTORRENT_PORT}" \
+          f"&uploaded=0&downloaded=0&left=16384&compact=1&event=started" \
+          f"&no_peer_id=1&numwant=200"
     failed = False
-    async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=7)) as session:
+    i = 5
+    err = None
+    while i > 0:
         try:
-            async with session.get(URL(url, encoded=True)) as response:
-                task = asyncio.ensure_future(response.read())
-                task_registry.add(task)
-                result = await task
-                if response.status != 200:
+            async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=7)) as session:
+                try:
+                    async with session.get(URL(url, encoded=True)) as response:
+                        task = asyncio.ensure_future(response.read())
+                        task_registry.add(task)
+                        result = await task
+                        if response.status != 200:
+                            failed = True
+                        task_registry.remove(task)
+                except (
+                    aiohttp.client_exceptions.ClientConnectorError,
+                    asyncio.TimeoutError,
+                    asyncio.CancelledError,
+                ):
                     failed = True
-                task_registry.remove(task)
-        except (
-            aiohttp.client_exceptions.ClientConnectorError,
-            asyncio.TimeoutError,
-            asyncio.CancelledError,
-        ):
-            failed = True
 
-    if failed:
+            if failed:
+                return tracker, {"seeders": 0, "leechers": 0, "peers": []}
+            result = bdecode(result)
+            break
+        except Exception as e:
+            i -= 1
+            time.sleep(5)
+            logger.error(f'Error {repr(e)}')
+            err = e
+    else:
+        logger.error(f'All tries failed. Error {repr(err)}')
         return tracker, {"seeders": 0, "leechers": 0, "peers": []}
 
-    result = bdecode(result)
     if b"failure reason" in result:
         return tracker, {"seeders": 0, "leechers": 0, "peers": []}
 
