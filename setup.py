780866d4935ad8b76c6d2176c8fbcd5939b5cda2
Anders Jensen <667117+JohnDoee@users.noreply.github.com>
2024-01-06 16:47:31 +0100

    bumped version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="magnet2torrent",
-    version="1.1.2",
+    version="1.2.0",
     url="https://github.com/JohnDoee/magnet2torrent",
     author="Anders Jensen",
     author_email="andersandjensen@gmail.com",
